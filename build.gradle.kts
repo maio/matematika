3a80ed335fc355760f5031948e3fb1e432b73965
Marian Schubert <marian.schubert@gmail.com>
2020-03-22 16:40:00 +0100

    Simple project with React

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("org.jetbrains.kotlin.js") version "1.3.70"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+    maven("https://kotlin.bintray.com/kotlin-js-wrappers/")
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-js"))
+
+    implementation("org.jetbrains:kotlin-react:16.13.0-pre.93-kotlin-1.3.70")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.0-pre.93-kotlin-1.3.70")
+    implementation(npm("react", "16.13.0"))
+    implementation(npm("react-dom", "16.13.0"))
+
+    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.94-kotlin-1.3.70")
+    implementation(npm("styled-components"))
+    implementation(npm("inline-style-prefixer"))
+
+    testImplementation(kotlin("test-js"))
+}
+
+kotlin.target.browser { }
